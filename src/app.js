34abd3eeec218d953dfdd1f7543c095fde71e197
Ekaterina Goryainova <kateas27@mail.ru>
2023-12-03 19:59:56 +0200

    merge develop in master

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import "./styles.css";
 const contextMenu = new ContextMenu(".menu");
 contextMenu.open();
 
-//добавляем background
+//добавляем модуль background
 const background = new BackgroundModule("background", "Изменить цвет фона");
 contextMenu.add(background);
 const backgroundItem = document.querySelector(`li[data-type="background"`);
@@ -15,7 +15,7 @@ backgroundItem.addEventListener("click", (event) => {
   background.trigger();
 });
 
-//добавляем clicks
+//добавляем модуль clicks
 const clicks = new ClicksModule("clicks", "Считать клики (5 секунд)");
 contextMenu.add(clicks);
 const clicksItem = document.querySelector(`li[data-type="clicks"`);
@@ -23,7 +23,7 @@ clicksItem.addEventListener("click", (event) => {
   clicks.trigger();
 });
 
-//добавляем таймер
+//добавляем модуль таймер
 const timer = new TimerModule("timer", "Таймер");
 contextMenu.add(timer);
 const timerItem = document.querySelector(`li[data-type="timer"`);
